bebcf84550018e07ecdf254c93135e1149964e7f
butschster <butschster@gmail.com>
2023-05-21 17:00:41 +0400

    Fixes HTTP frame payload

--- src/Proto/Frame/Http.php
+++ src/Proto/Frame/Http.php
@@ -8,6 +8,7 @@ use Buggregator\Client\Proto\Frame;
 use Buggregator\Client\ProtoType;
 use DateTimeImmutable;
 use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Message\UploadedFileInterface;
 
 final class Http extends Frame
 {
@@ -23,6 +24,27 @@ final class Http extends Frame
      */
     public function __toString(): string
     {
-        return \json_encode($this->request, JSON_THROW_ON_ERROR);
+        return \json_encode([
+            'headers' => $this->request->getHeaders(),
+            'method' => $this->request->getMethod(),
+            'uri' => (string)$this->request->getUri(),
+            'body' => (string)$this->request->getBody(),
+            'serverParams' => $this->request->getServerParams(),
+            'cookies' => $this->request->getCookieParams(),
+            'queryParams' => $this->request->getQueryParams(),
+            'parsedBody' => $this->request->getParsedBody(),
+            'attributes' => $this->request->getAttributes(),
+            'protocolVersion' => $this->request->getProtocolVersion(),
+            'uploadedFiles' => \array_map(
+                static fn(UploadedFileInterface $file) => [
+                    'clientFilename' => $file->getClientFilename(),
+                    'clientMediaType' => $file->getClientMediaType(),
+                    'error' => $file->getError(),
+                    'size' => $file->getSize(),
+                    'content' => (string)$file->getStream(),
+                ],
+                $this->request->getUploadedFiles()
+            ),
+        ], JSON_THROW_ON_ERROR);
     }
 }
