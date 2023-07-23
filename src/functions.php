073087947642e48c1d0a6bdcf4ae8aa30f62a34f
roxblnfk <roxblnfk@ya.ru>
2023-07-23 20:45:24 +0400

    Add the `trap()` function that converts protobuf messages for dumping and configures $_SERVER to use server on localhost by default; add var-dumper in dependencies

---
+++ src/functions.php
@@ -0,0 +1,67 @@
+<?php
+
+declare(strict_types=1);
+
+use Buggregator\Client\Support\ProtobufDTO;
+use Google\Protobuf\Internal\Message;
+use Symfony\Component\VarDumper\Caster\TraceStub;
+use Symfony\Component\VarDumper\VarDumper;
+
+if (!function_exists('trap')) {
+    /**
+     * Configure VarDumper to dump values to the local server.
+     * If there are no values - dump stack trace.
+     *
+     * @param mixed ...$values
+     */
+    function trap(mixed ...$values): void
+    {
+        // Set default values if not set
+        if (!isset($_SERVER['VAR_DUMPER_FORMAT'], $_SERVER['VAR_DUMPER_SERVER'])) {
+            $_SERVER['VAR_DUMPER_FORMAT'] = 'server';
+            // todo use the config file in the future
+            $_SERVER['VAR_DUMPER_SERVER'] = '127.0.0.1:9912';
+        }
+
+        // If there are no values - stack trace
+        if ($values === []) {
+            // VarDumper::dump(\debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS));
+            $cwd = \getcwd();
+            $stack = \debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS);
+            // Replace paths with relative paths
+            foreach ($stack as $i => $frame) {
+                if (isset($frame['file'])) {
+                    $stack[$i]['file'] = \str_replace($cwd, '.', $frame['file']);
+                }
+            }
+            VarDumper::dump([
+                'cwd' => $cwd,
+                'trace' => new TraceStub($stack)
+            ]);
+            return;
+        }
+
+        // Process protobuf classes
+        foreach ($values as $key => $value) {
+            try {
+                if ($value instanceof Message) {
+                    $values[$key] = ProtobufDTO::createFromMessage($value);
+                }
+            } catch (\Throwable) {
+                // Ignore
+            }
+        }
+
+        // Dump single value
+        if (array_keys($values) === [0]) {
+            VarDumper::dump($values);
+            return;
+        }
+
+        // Dump sequence of values
+        foreach ($values as $key => $value) {
+            VarDumper::dump($value, $key);
+		}
+    }
+}
+
---
+++ tests/Support/ProtobufDTOTest.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Buggregator\Client\Tests\Support;
+
+use Buggregator\Client\Support\ProtobufDTO;
+use PHPUnit\Framework\TestCase;
+
+class ProtobufDTOTest extends TestCase
+{
+    public function testCreateFromFalseMessage()
+    {
+        $message = new \Google\Protobuf\BoolValue();
+
+        $dto = ProtobufDTO::createFromMessage($message);
+
+        $this->assertSame('google.protobuf.BoolValue', $dto->name);
+        $this->assertSame(\Google\Protobuf\BoolValue::class, $dto->class);
+        $this->assertFalse($dto->value);
+    }
+
+    public function testCreateFromTrueMessage()
+    {
+        $message = (new \Google\Protobuf\BoolValue())->setValue(true);
+
+        $dto = ProtobufDTO::createFromMessage($message);
+
+        $this->assertSame('google.protobuf.BoolValue', $dto->name);
+        $this->assertSame(\Google\Protobuf\BoolValue::class, $dto->class);
+        $this->assertTrue($dto->value);
+    }
+
+    public function testCreateFromTimestampMessage()
+    {
+        $message = (new \Google\Protobuf\Timestamp())->setSeconds(1234567890)->setNanos(1234567890);
+
+        $dto = ProtobufDTO::createFromMessage($message);
+
+        $this->assertSame('google.protobuf.Timestamp', $dto->name);
+        $this->assertSame(\Google\Protobuf\Timestamp::class, $dto->class);
+        $this->assertSame('2009-02-13T23:31:30.1234567890Z', $dto->value);
+    }
+
+    public function testCreateFromDurationMessage()
+    {
+        $message = (new \Google\Protobuf\Duration())->setSeconds(1234567890)->setNanos(1234567890);
+
+        $dto = ProtobufDTO::createFromMessage($message);
+
+        $this->assertSame('google.protobuf.Duration', $dto->name);
+        $this->assertSame(\Google\Protobuf\Duration::class, $dto->class);
+        $this->assertSame('1234567891.234567890s', $dto->value);
+    }
+}
