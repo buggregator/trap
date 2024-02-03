c3ed1747e6e039e0407ec5e5857125ab0f00052e
roxblnfk <roxblnfk@ya.ru>
2024-02-03 18:05:10 +0400

    Update frontend files

--- resources/frontend/_nuxt/use-smtp.6e0c9e5e.js
+++
@@ -1,1 +0,0 @@
-import{O as t}from"./entry.d63b9a39.js";const m=a=>({id:a.uuid,type:t.SMTP,labels:[t.SMTP],origin:null,serverName:"",date:a.timestamp?new Date(a.timestamp*1e3):null,payload:a.payload}),l=()=>({normalizeSmtpEvent:m});export{l as u};
