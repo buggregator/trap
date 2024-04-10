d927ec75b027ac0abb9ff670604272d4c4463284
roxblnfk <roxblnfk@ya.ru>
2024-04-10 18:28:54 +0400

    Correct buffer size in the HTTP Emitter; remove artificial socket close timers

--- src/Socket/Client.php
+++ src/Socket/Client.php
@@ -98,8 +98,9 @@ final class Client implements Destroyable
             }
 
             if ($this->toDisconnect && $this->writeQueue === []) {
-                // Wait for the socket buffer to be flushed.
-                (new Timer(0.005))->wait();
+                # Wait for the socket buffer to be flushed.
+                // (new Timer(0.005))->wait();
+
                 throw new ClientDisconnected();
             }
             Fiber::suspend();
