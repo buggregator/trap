6bd698580d0c8de44e3f5890ac122c0dee1f6aae
roxblnfk <roxblnfk@ya.ru>
2024-04-14 17:53:08 +0400

    Update frontend

---
+++ resources/frontend/_nuxt/blank.084ffbed.js
@@ -0,0 +1,1 @@
+import{_ as t,a as _,D as n,o as s}from"./entry.8cc9a0a4.js";const a={},o={class:"main-layout"};function c(e,r){return s(),_("div",o,[n(e.$slots,"default",{},void 0,!0)])}const l=t(a,[["render",c],["__scopeId","data-v-7513e831"]]);export{l as default};
