018d28de47f729c9dc43a28e548c9992370eaee9
l0gic <l0gic@ya.ru>
2024-04-30 08:10:21 +0500

    Revert "Remove unused Info::LOGO_CLI_COLOR"
    This reverts commit f8863d3bfd319e9c2f12a06c0e826a0c9a8ba93d.

--- src/Info.php
+++ src/Info.php
@@ -11,5 +11,20 @@ class Info
 {
     public const NAME = 'Buggregator Trap';
     public const VERSION = '1.4.6';
+    public const LOGO_CLI_COLOR = <<<CONSOLE
+        \e[44;97;1m                                    \e[0m
+        \e[44;97;1m      ▄█▀                  ▀█▄      \e[0m
+        \e[44;97;1m     ▐█▌      ▄█▀▀▀▀█▄      ▐█▌     \e[0m
+        \e[44;97;1m     ██   ▀▄ █        █ ▄▀   ██     \e[0m
+        \e[44;97;1m    ▐█▌     █▀▀▀▀▀▀▀▀▀▀█     ▐█▌    \e[0m
+        \e[44;97;1m   ▄█▀     ▐▌  ▀▄  ▄▀  ▐▌     ▀█▄   \e[0m
+        \e[44;97;1m   ▀█▄   ▄▄█     ██     █▄▄   ▄█▀   \e[0m
+        \e[44;97;1m    ▐█▌     █  ▄▀  ▀▄  █     ▐█▌    \e[0m
+        \e[44;97;1m     ██      █▄      ▄█      ██     \e[0m
+        \e[44;97;1m     ▐█▌   ▄▀  ▀▄▄▄▄▀  ▀▄   ▐█▌     \e[0m
+        \e[44;97;1m      ▀█▄                  ▄█▀      \e[0m
+        \e[44;97;1m                                    \e[0m
+        CONSOLE;
+
     public const TRAP_ROOT = __DIR__ . '/..';
 }
