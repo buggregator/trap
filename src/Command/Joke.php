687a18c5bc5f7bb1faceb0094eedadec13a0400c
roxblnfk <roxblnfk@ya.ru>
2024-05-04 13:57:35 +0400

    Add more joke phrases

--- src/Command/Joke.php
+++ src/Command/Joke.php
@@ -4,19 +4,18 @@ declare(strict_types=1);
 
 namespace Buggregator\Trap\Command;
 
+use Buggregator\Trap\Info;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
 /**
- * Print joke
- *
  * @internal
  */
 #[AsCommand(
     name: 'joke',
-    description: 'Print joke',
+    description: 'Print a joke',
 )]
 final class Joke extends Command
 {
@@ -24,8 +23,12 @@ final class Joke extends Command
         InputInterface $input,
         OutputInterface $output,
     ): int {
-        $jokes = \file(filename: 'resources/registry/jokes.txt', flags: \FILE_SKIP_EMPTY_LINES);
-        $joke = \base64_decode($jokes[\array_rand($jokes)]);
+        $jokes = \file(
+            filename: Info::TRAP_ROOT . '/resources/registry/jokes.txt',
+            flags: \FILE_IGNORE_NEW_LINES | \FILE_IGNORE_NEW_LINES,
+        );
+        $joke = \str_replace('%s', 'Buggregator', \base64_decode($jokes[\array_rand($jokes)]));
+
         \trap($joke);
 
         return Command::SUCCESS;
