0c97ebc7ff3f5ba04cce6275e6058c2db06b9b3a
roxblnfk <roxblnfk@ya.ru>
2024-05-07 17:02:59 +0400

    test CS CI

--- src/Application.php
+++ src/Application.php
@@ -198,9 +198,9 @@ final class Application implements Processable, Cancellable, Destroyable
     /**
      * @param int<1, 65535> $port
      */
-    public function configureFrontend(int $port): void
+    public function configureFrontend(): void
     {
-        $this->senders[] = $wsSender = Sender\FrontendSender::create($this->logger);
+        $this->senders [ ] = $wsSender = Sender\FrontendSender::create($this->logger);
 
         $inspector = $this->container->make(Inspector::class, [
             new Traffic\Dispatcher\Http(
@@ -215,8 +215,9 @@ final class Application implements Processable, Cancellable, Destroyable
             ),
         ]);
         $this->processors[] = $inspector;
-        $this->processors[] = $wsSender;
-        $config = $this->container->get(FrontendConfig::class);
+        $this->processors[] =  $wsSender;
+        $config = $this->container->get( FrontendConfig::class);
         $this->prepareServerFiber(new SocketServer(port: $config->port), $inspector, $this->logger);
     }
 }
+
