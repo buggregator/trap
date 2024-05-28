cd02befbe1b229d623b26b09b97b30e121e1bcfc
roxblnfk <roxblnfk@ya.ru>
2024-05-28 13:21:28 +0400

    test: cover `tr()` function

--- tests/Unit/Client/FunctionTrapTest.php
+++ tests/Unit/Client/FunctionTrapTest.php
@@ -4,15 +4,14 @@ declare(strict_types=1);
 
 namespace Buggregator\Trap\Tests\Unit\Client;
 
+use PHPUnit\Framework\Attributes\Group;
+use PHPUnit\Framework\Attributes\RunInSeparateProcess;
 use PHPUnit\Framework\TestCase;
 
 final class FunctionTrapTest extends TestCase
 {
-    /**
-     * @runInSeparateProcess
-     *
-     * @group phpunit-only
-     */
+    #[RunInSeparateProcess]
+    #[Group('phpunit-only')]
     public function testLeak(): void
     {
         $object = new \stdClass();
