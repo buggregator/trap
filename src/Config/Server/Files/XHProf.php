5939b7d5f9c1789c1dde339c6d51919d482ed3d0
roxblnfk <roxblnfk@ya.ru>
2024-06-01 18:04:29 +0400

    chore: add short comment for the TRAP_XHPROF_SORT env; update composer.lock

--- src/Config/Server/Files/XHProf.php
+++ src/Config/Server/Files/XHProf.php
@@ -13,9 +13,16 @@ use Buggregator\Trap\Service\FilesObserver\Converter\XHProf as Converter;
  */
 final class XHProf extends ObserverConfig
 {
-    /** @var int<0, 3> Edges sorting algorithm */
+    /**
+     * @var int<0, 3> Edges sorting algorithm
+     *      Where:
+     *        0 - Deep-first
+     *        1 - Deep-first with sorting by WT
+     *        2 - Level-by-level
+     *        3 - Level-by-level with sorting by WT
+     */
     #[Env('TRAP_XHPROF_SORT')]
-    public int $algorithm = 1;
+    public int $algorithm = 3;
 
     /** @var non-empty-string|null Path to XHProf files */
     #[Env('TRAP_XHPROF_PATH')]
