b84ca7caf5d80ae17c926349a54fb80c5a5395e3
roxblnfk <roxblnfk@ya.ru>
2024-06-02 12:27:30 +0400

    ci: use pest for arch tests only; phpunit for unit tests without limitations

--- tests/Unit/Client/FunctionTrapTest.php
+++ tests/Unit/Client/FunctionTrapTest.php
@@ -4,14 +4,12 @@ declare(strict_types=1);
 
 namespace Buggregator\Trap\Tests\Unit\Client;
 
-use PHPUnit\Framework\Attributes\Group;
 use PHPUnit\Framework\Attributes\RunInSeparateProcess;
 use PHPUnit\Framework\TestCase;
 
 final class FunctionTrapTest extends TestCase
 {
     #[RunInSeparateProcess]
-    #[Group('phpunit-only')]
     public function testLeak(): void
     {
         $object = new \stdClass();
