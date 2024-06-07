63a29908f5b904f1b438f1e867a56c5fd00d98d3
roxblnfk <roxblnfk@ya.ru>
2024-06-07 19:08:29 +0400

    feat(smtp): detect embedded files and solve links inside html body

--- src/Sender/Frontend/Mapper/Smtp.php
+++ src/Sender/Frontend/Mapper/Smtp.php
@@ -8,6 +8,7 @@ use Buggregator\Trap\Proto\Frame\Smtp as SmtpFrame;
 use Buggregator\Trap\Sender\Frontend\Event;
 use Buggregator\Trap\Support\Uuid;
 use Buggregator\Trap\Traffic\Message\Multipart\File;
+use Buggregator\Trap\Traffic\Message\Smtp as SmtpMessage;
 use Buggregator\Trap\Traffic\Message\Smtp\MessageFormat;
 
 /**
@@ -19,11 +20,11 @@ final class Smtp
     {
         $message = $frame->message;
 
-        /** @var \ArrayAccess<non-empty-string, Event\Asset> $assets */
-        $assets = new \ArrayObject();
+        $uuid = Uuid::generate();
+        $assets = self::fetchAssets($message);
 
         return new Event(
-            uuid: $uuid = Uuid::generate(),
+            uuid: $uuid,
             type: 'smtp',
             payload: [
                 'from' => $message->getSender(),
@@ -33,30 +34,88 @@ final class Smtp
                 'cc' => $message->getCc(),
                 'bcc' => $message->getBcc(),
                 'text' => $message->getMessage(MessageFormat::Plain)?->getValue() ?? '',
-                'html' => $message->getMessage(MessageFormat::Html)?->getValue() ?? '',
+                'html' => self::html($uuid, $message, $assets),
                 'raw' => (string) $message->getBody(),
-                'attachments' => \array_map(
-                    static function (File $attachment) use ($assets, $uuid): array {
-                        $asset = new Event\AttachedFile(
-                            id: Uuid::generate(),
-                            file: $attachment,
-                        );
-                        $uri = $uuid . '/' . $asset->uuid;
-                        $assets->offsetSet($asset->uuid, $asset);
-
-                        return [
-                            'id' => $asset->uuid,
-                            'name' => $attachment->getClientFilename(),
-                            'uri' => $uri,
-                            'size' => $attachment->getSize(),
-                            'mime' => $attachment->getClientMediaType(),
-                        ];
-                    },
-                    $message->getAttachments(),
-                ),
             ],
             timestamp: (float) $frame->time->format('U.u'),
             assets: $assets,
         );
+    }
+
+    /**
+     * @return \ArrayAccess<non-empty-string, Event\Asset>
+     */
+    private static function fetchAssets(SmtpMessage $message): \ArrayAccess
+    {
+        /** @var \ArrayAccess<non-empty-string, Event\Asset> $assets */
+        $assets = new \ArrayObject();
+
+        foreach ($message->getAttachments() as $attachment) {
+            $asset = self::asset($attachment);
+            $assets->offsetSet($asset->uuid, $asset);
+        }
+
+        return $assets;
+    }
+
+    /**
+     * @param non-empty-string $uuid UUID of the event
+     */
+    private static function assetLink(string $uuid, Event\Asset $asset): string
+    {
+        return "/api/smtp/$uuid/attachment/$asset->uuid";
+    }
+
+    private static function asset(File $attachment): Event\Asset
+    {
+        /**
+         * Detect if the file is an embedded image
+         *
+         * @var non-empty-string|null $embedded
+         */
+        $embedded = match (true) {
+            // Content-Disposition is inline and name is present
+            \str_starts_with($attachment->getHeaderLine('Content-Disposition'), 'inline') && \preg_match(
+                '/name=(?:\"([^\"]++)\"|\'([^\']++)\'|([^;,\\s]++))/',
+                $attachment->getHeaderLine('Content-Disposition'),
+                $matches,
+            ) === 1 => $matches[1],
+
+            // Content-Type is image/* and has name
+            \str_starts_with($attachment->getHeaderLine('Content-Type'), 'image/') && \preg_match(
+                '/name=(?:\"([^\"]++)\"|\'([^\']++)\'|([^;,\\s]++))/',
+                $attachment->getHeaderLine('Content-Type'),
+                $matches,
+            ) === 1 => $matches[1],
+            default => null,
+        };
+
+        return $embedded === null
+            ? new Event\AttachedFile(
+                id: Uuid::generate(),
+                file: $attachment,
+            )
+            : new Event\EmbeddedFile(
+                id: Uuid::generate(),
+                file: $attachment,
+                name: $embedded,
+            );
+    }
+
+    /**
+     * @param non-empty-string $uuid
+     * @param \ArrayAccess<non-empty-string, Event\Asset> $assets
+     */
+    private static function html(string $uuid, SmtpMessage $message, \ArrayAccess $assets): string
+    {
+        $result = $message->getMessage(MessageFormat::Html)?->getValue() ?? '';
+
+        // Replace CID links with actual asset links
+        $toReplace = [];
+        foreach ($assets as $asset) {
+            $asset instanceof Event\EmbeddedFile and $toReplace["cid:{$asset->name}"] = self::assetLink($uuid, $asset);
+        }
+
+        return \str_replace(\array_keys($toReplace), \array_values($toReplace), $result);
     }
 }
