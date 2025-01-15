f87a70e72b5c62546a3f8f6ed8e20b9e4e219ef7
Alexander Strizhak <gam6itko@gmail.com>
2025-01-15 20:15:25 +0300

    MailToFileSender

---
+++ src/Sender/MailToFileSender.php
@@ -0,0 +1,46 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Buggregator\Trap\Sender;
+
+use Buggregator\Trap\Proto\Frame;
+use Buggregator\Trap\Proto\Frame\Smtp;
+use Buggregator\Trap\Sender;
+
+/**
+ * @internal
+ */
+class MailToFileSender implements Sender
+{
+    private readonly string $path;
+
+    public function __construct(
+        string $path = 'runtime/mail',
+    ) {
+        $this->path = \rtrim($path, '/\\');
+        if (!\is_dir($path) && !\mkdir($path, 0o777, true) && !\is_dir($path)) {
+            throw new \RuntimeException(\sprintf('Directory "%s" was not created', $path));
+        }
+    }
+
+    public function send(iterable $frames): void
+    {
+        /** @var Frame $frame */
+        foreach ($frames as $frame) {
+            if (!$frame instanceof Smtp) {
+                continue;
+            }
+
+            foreach ($frame->message->getBcc() as $bcc) {
+                $path = $this->path . DIRECTORY_SEPARATOR . $bcc->email;
+                if (!\is_dir($path) && !\mkdir($path, 0o777, true) && !\is_dir($path)) {
+                    throw new \RuntimeException(\sprintf('Directory "%s" was not created', $path));
+                }
+                $filepath = \sprintf("%s/%s.json", $path, \date('Y-m-d-H-i-s-v'));
+                \assert(!\file_exists($filepath));
+                \file_put_contents($filepath, \json_encode($frame->message, \JSON_THROW_ON_ERROR));
+            }
+        }
+    }
+}
