ff69ea045f241289bf64353117e1e90e0d47d0ad
roxblnfk <roxblnfk@ya.ru>
2025-01-15 23:11:37 +0400

    test: fix MailToFileSender tests

--- tests/Unit/Sender/MailToFileSenderTest.php
+++ tests/Unit/Sender/MailToFileSenderTest.php
@@ -4,9 +4,11 @@ declare(strict_types=1);
 
 namespace Buggregator\Trap\Tests\Unit\Sender;
 
+use Buggregator\Trap\Info;
 use Buggregator\Trap\Proto\Frame\Smtp as SmtpFrame;
 use Buggregator\Trap\Sender\MailToFileSender;
 use Buggregator\Trap\Traffic\Message\Smtp as SmtpMessage;
+use DirectoryIterator;
 use PHPUnit\Framework\TestCase;
 
 /**
@@ -14,11 +16,12 @@ use PHPUnit\Framework\TestCase;
  */
 final class MailToFileSenderTest extends TestCase
 {
+    /** @var list<non-empty-string> */
     private array $cleanupFolders = [];
 
     public function testForSmtp(): void
     {
-        $this->cleanupFolders[] = $root = \sys_get_temp_dir() . DIRECTORY_SEPARATOR . \uniqid('trap_mail_');
+        $this->cleanupFolders[] = $root = Info::TRAP_ROOT . '/runtime/tests/mail-to-file-sender';
 
         $message = SmtpMessage::create(
             protocol: [
@@ -36,23 +39,23 @@ final class MailToFileSenderTest extends TestCase
         $sender = new MailToFileSender($root);
         $sender->send([$frame]);
 
-        $this->assertRecipient("$root/user1@company.tld");
-        $this->assertRecipient("$root/user2@company.tld");
+        $this->assertRecipient("$root/user1[at]company.tld");
+        $this->assertRecipient("$root/user2[at]company.tld");
     }
 
     protected function tearDown(): void
     {
         foreach ($this->cleanupFolders as $folder) {
-            \array_map('unlink', \glob("$folder/*.*"));
+            \array_map('unlink', \glob("$folder/*/*.*"));
+            \array_map('rmdir', \glob("$folder/*"));
             \rmdir($folder);
         }
     }
 
     private function assertRecipient(string $folder): void
     {
-        self::assertTrue(\file_exists($folder));
-        self::assertTrue(\is_dir($folder));
-        $files = \glob("$folder/*.json");
+        self::assertDirectoryExists($folder);
+        $files = \glob(\str_replace('[', '[[]', "$folder/*.json"));
         self::assertCount(1, $files);
         $arr = \json_decode(\file_get_contents($files[0]), true, \JSON_THROW_ON_ERROR);
         self::assertArrayHasKey('protocol', $arr);
