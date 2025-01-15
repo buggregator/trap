a60751c2c3528646d56e227b0f36de55903092cd
Alexander Strizhak <gam6itko@gmail.com>
2025-01-15 20:16:54 +0300

    up

--- src/Sender/MailToFileSender.php
+++ src/Sender/MailToFileSender.php
@@ -17,7 +17,8 @@ class MailToFileSender implements Sender
 
     public function __construct(
         string $path = 'runtime/mail',
-    ) {
+    )
+    {
         $this->path = \rtrim($path, '/\\');
         if (!\is_dir($path) && !\mkdir($path, 0o777, true) && !\is_dir($path)) {
             throw new \RuntimeException(\sprintf('Directory "%s" was not created', $path));
@@ -33,6 +34,10 @@ class MailToFileSender implements Sender
             }
 
             foreach ($frame->message->getBcc() as $bcc) {
+                if (null === $bcc->email) {
+                    continue;
+                }
+
                 $path = $this->path . DIRECTORY_SEPARATOR . $bcc->email;
                 if (!\is_dir($path) && !\mkdir($path, 0o777, true) && !\is_dir($path)) {
                     throw new \RuntimeException(\sprintf('Directory "%s" was not created', $path));
